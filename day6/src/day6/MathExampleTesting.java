057a822736796710ce00756f5f2a175fd4531598
Navaneeth Kumar <knavaneeth865@gmail.com>
2026-03-17 17:03:28 +0530

    junit testing basics

--- day6/src/day6/MathExampleTesting.java
+++ day6/src/day6/MathExampleTesting.java
@@ -1,9 +1,16 @@
 package day6;
 
+import java.util.stream.Stream;
+
 public class MathExampleTesting {
 
 int add(int a,int b) {
 	return a+b;
-
+}
+int addWithArray(Integer[] numbers) {
+	return Stream.of(numbers).reduce(0,(current,element)-> current+element);
+}
+int divide(int a,int b) {
+	return a/b;
 }
 }
--- day6/src/day6/MathExampleTestingTest.java
+++ day6/src/day6/MathExampleTestingTest.java
@@ -1,14 +1,59 @@
 package day6;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathExampleTestingTest {
+	MathExampleTesting math;
+
+	@BeforeEach
+	void setup() {
+		math = new MathExampleTesting();
+	}
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+
+		int result = math.add(3, 5);// act
+		assertEquals(8, result);// assert
+
 	}
 
+	@Test
+	void testAddNegativeNumbers() {
+
+		int result = math.add(-2, -5);// act
+		assertEquals(-7, result);// assert
+	}
+
+	@Test
+	void testAddNegativeAndPositveNumbers() {
+		int result = math.add(2, -5);// act
+		assertEquals(-3, result);// assert
+	}
+
+	// 2nd method
+	@Test
+	void testWithArray() {
+		int result = math.addWithArray(new Integer[] { 1, 2, 3, 4, 5 });
+		assertEquals(15, result);
+	}
+
+	@Test
+	void testdivide() {
+		int result = math.divide(4, 2);
+		assertEquals(2, result);
+	}
+
+
+//	void testWithZero() {
+//		//assertThrows(ArithmeticException.class, () -> {
+//			math.divide(20, 0);
+//		});
+//	}
+	// if junit testing shows failure,it means,test case failure,it it shows
+	// error,code is not working
 }
