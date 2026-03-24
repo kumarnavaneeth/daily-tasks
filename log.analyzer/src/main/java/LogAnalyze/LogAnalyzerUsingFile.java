63ffb3017d13d031ba2416dcb8e283a7462dfd7f
Navaneeth Kumar <knavaneeth865@gmail.com>
2026-03-24 15:12:40 +0530

    learning-pr-2

--- log.analyzer/src/main/java/LogAnalyze/LogAnalyzerUsingFile.java
+++ log.analyzer/src/main/java/LogAnalyze/LogAnalyzerUsingFile.java
@@ -13,6 +13,8 @@ public static void main(String[] args) throws IOException {
 	String line;
 	while((line=reader.readLine())!=null){
 		map.put(Integer, line);
+		System.out.println();
+		System.out.println();
 	}
 }
 }
